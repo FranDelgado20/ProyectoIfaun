b4a957a2c1dfff8e4fee43042af3fec9487f28b9
SantiJaime <santij03@gmail.com>
2024-01-18 15:36:46 -0300

    modifico esquema de validación

--- src/utils/validationSchemaError.js
+++ src/utils/validationSchemaError.js
@@ -1,22 +1,22 @@
 import * as yup from "yup";
 export const errorRegister = yup.object().shape({
-  fullName: yup.string().required("Este campo es OBLIGATORIO"),
+  fullName: yup.string().required("Campo nombre y apellido obligatorio"),
 
   email: yup
     .string()
-    .required("Este campo es OBLIGATORIO")
-    .email("Formato incorrecto: debe contener @"),
-  pass: yup.string().required("Este campo es OBLIGATORIO"),
-  rPass: yup.string().required("Este campo es OBLIGATORIO"),
+    .required("Campo correo electrónico obligatorio")
+    .email("Formato de correo electrónico inválido"),
+  pass: yup.string().required("Campo contraseña obligatorio"),
+  rPass: yup.string().required("Campo repetir contraseña obligatorio"),
   numberPhone: yup
     .string()
-    .matches(/^\+54\d{3}\d{7}$/, "Formato Incorrecto")
-    .required("Este campo es OBLIGATORIO"),
+    .matches(/^\+54\d{3}\d{7}$/, "Formato de número de teléfono inválido")
+    .required("Campo número de teléfono obligatorio"),
 });
 export const errorLogin = yup.object().shape({
   email: yup
     .string()
-    .required("Este campo es OBLIGATORIO")
-    .email("Formato incorrecto: debe contener @"),
-  pass: yup.string().required("Este campo es OBLIGATORIO"),
+    .required("Campo correo electrónico obligatorio")
+    .email("Formato de correo electrónico inválido"),
+  pass: yup.string().required("Campo contraseña obligatorio"),
 });
