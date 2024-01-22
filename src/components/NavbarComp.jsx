350c0a5444a7e05a0656e044fb25b3e404f13dae
FranDelgado20 <delgadofran78@gmail.com>
2024-01-22 15:06:20 -0300

    modificaciones en navbarComp y realizo logica para iniciar sesion

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,41 +1,114 @@
+import { useEffect } from "react";
 import {
-    Navbar,
-    Nav,
-    Container,
-    NavDropdown,
-  } from "react-bootstrap";
-  import { NavLink } from "react-router-dom";
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Button,
+  Dropdown,
+} from "react-bootstrap";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NavbarComp = () => {
+  const navigate = useNavigate();
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const role = JSON.parse(sessionStorage.getItem("role"));
+  const logOut = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("role");
+    sessionStorage.removeItem("idUser");
+    navigate("/");
+  };
+  useEffect(() => {
+    const token = JSON.parse(sessionStorage.getItem("token"));
+  });
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          <img src="/logoIfaun.png" alt="Ifaun Logo" height={"70px"}/>
+          <img src="/logoIfaun.png" alt="Ifaun Logo" height={"70px"} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse 
-        id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link as={NavLink} to="/materias" >
-              Materias
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/blog">
-              Blog
-            </Nav.Link>
-          </Nav>
-          <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/login" >
-              Iniciar Sesion
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/register">
-              Registrarse
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        {token && role === "user" ? (
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Dropdown>
+                <Dropdown.Toggle
+                  className="nav-link"
+                  variant="light"
+                  id="dropdown-basic"
+                >
+                  <i class="bi bi-book"></i>
+                  Materias
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Nav.Link as={NavLink} to="/blog">
+                <i class="bi bi-file-text"></i>
+                Blog
+              </Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              <Nav.Link as={NavLink} to="/miCuenta">
+                <i className="bi bi-person"> </i>
+                Mi Cuenta
+              </Nav.Link>
+              <Button onClick={logOut} className="nav-link">
+                {" "}
+                <i className="bi bi-door-open-fill"></i>Cerrar Sesión
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        ) : token && role === "admin" ? (
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* <Nav>
+              <Nav.Link as={NavLink} to="/materias">
+                Materias
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/blog">
+                Blog
+              </Nav.Link>
+            </Nav> */}
+            <Nav className="ms-auto">
+              <Button onClick={logOut} className="nav-link">
+                {" "}
+                <i className="bi bi-door-open-fill"></i>Cerrar Sesión
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        ) : (
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link as={NavLink} to="/materias">
+                Materias
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/blog">
+                Blog
+              </Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              <Nav.Link as={NavLink} to="/login">
+                Iniciar Sesion
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/register">
+                Registrarse
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        )}
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavbarComp
+export default NavbarComp;
