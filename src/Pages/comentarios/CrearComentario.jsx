c4fb889cedccce3a67c3930ee134c1f917ed753d
Tomas <tomibudeguer20@gmail.com>
2024-01-25 18:33:08 -0300

    agregue fuentes y formulario para comentarios

--- src/Pages/comentarios/CrearComentario.jsx
+++ src/Pages/comentarios/CrearComentario.jsx
@@ -17,9 +17,10 @@ const CrearComentario = ({ setComentarios }) => {
   } = useForm({
     defaultValues: {
       nombreUsuario: "Jaimassso",
-      fotoDePerfil: "https://i.pinimg.com/236x/2f/97/f0/2f97f05b32547f54ef1bdf99cd207c90.jpg",
+      fotoDePerfil:
+        "https://i.pinimg.com/236x/2f/97/f0/2f97f05b32547f54ef1bdf99cd207c90.jpg",
       comentario: "",
-      fecha: dia.toLocaleDateString()
+      fecha: dia.toLocaleDateString(),
     },
   });
 
@@ -29,44 +30,43 @@ const CrearComentario = ({ setComentarios }) => {
     //   Swal.fire("Debe iniciar sesion!", "Error al enviar comentario!", "error");
     //   navegacion("/login");
     // } else {
-      crearComentario(datos).then((respuesta) => {
-        if (respuesta.status === 201) {
-          listarComentariosMostrables().then((respuesta) => {
-            setComentarios(respuesta);
-          });
-          Swal.fire(
-            "Gracias por tu comentario!",
-            "Su comenario fue enviado correctamente",
-            "success"
-          );
-          reset();
-        } else {
-          Swal.fire(
-            "Ocurrio un error",
-            "Vuelva a intentarlo mas tarde",
-            "error"
-          );
-        }
-      });
+    crearComentario(datos).then((respuesta) => {
+      if (respuesta.status === 201) {
+        listarComentariosMostrables().then((respuesta) => {
+          setComentarios(respuesta);
+        });
+        Swal.fire(
+          "Gracias por tu comentario!",
+          "Su comenario fue enviado correctamente",
+          "success"
+        );
+        reset();
+      } else {
+        Swal.fire("Ocurrio un error", "Vuelva a intentarlo mas tarde", "error");
+      }
+    });
     // }
   };
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Row>
         <Col sm={12}>
+          <h3>Envia tu comentario</h3>
+          <hr />
           <Form.Group className="mb-2" controlId="formNombre">
+            <Form.Label>Comentario</Form.Label>
             <Form.Control
               type="text"
               placeholder="Escriba su comentario"
               {...register("comentario", {
                 required: "Este dato es obligatorio",
                 minLength: {
-                  value: 2,
-                  message: "Debe ingresar como minimo 2 caracteres",
+                  value: 10,
+                  message: "Debe ingresar como minimo 10 caracteres",
                 },
                 maxLength: {
-                  value: 200,
-                  message: "Debe ingresar como maximo 200 caracteres",
+                  value: 300,
+                  message: "Debe ingresar como maximo 300 caracteres",
                 },
               })}
             />
