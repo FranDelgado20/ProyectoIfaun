d0557abc2f51d80d1d86552684abc6e2bd47e800
FranDelgado20 <delgadofran78@gmail.com>
2024-02-02 11:17:26 -0300

    modificaciones en css y pagina de error

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -4,16 +4,29 @@ import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <Container className="error-page text-center">
-      <div className="error-content">
-        <Image src="/error.png" fluid />
-        <Link to={"/"}>
-          <Button variant="info" className="error-button">
-            Volver al inicio
-          </Button>
-        </Link>
-      </div>
-    </Container>
+    <>
+      <Container className="error-page text-center pc-only">
+        <div className="error-content">
+          <Image src="/error.png" fluid />
+
+          <Link to={"/"}>
+            <Button className="button_modify_error" >
+              Volver al inicio
+            </Button>
+          </Link>
+        </div>
+      </Container>
+      <Container className="mobile-only" fluid>
+        <div className="error-content error-content-mobile">
+          <Image src="/IFAUN (2).png" fluid />
+          <Link to={"/"}>
+            <Button className="button_modify_error_mobile">
+              Volver al inicio
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
   );
 };
 export default Error404;
